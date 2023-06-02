bc2c6c42f7645c0dde0830a66005827eddd78333
akshat123436 <akshat123436@gmail.com>
2023-06-02 09:30:26 +0530

    ready to work locally

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -3,7 +3,6 @@ import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import CodeIcon from "@material-ui/icons/Code";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 const About = () => {
   const visitInstagram = () => {
